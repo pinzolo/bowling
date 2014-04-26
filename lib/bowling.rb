0dd73586c0b9b4aadf80d22b2148870e53d2a613
ice <meltedise@gmail.com>
2014-04-26 14:36:26 +0900

    add gutter calculation.

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,5 +1,36 @@
 # -*- coding: utf-8 -*-
 
+# game
+# frame
+# score
+
 class Bowling
-  
+  # == Parameters:
+  #
+  # game: "[[1,1],[2,3],...]"
+  #
+  # ex.
+  # [[5,3],[7,2],[8,/],[X],[7,1],[9,-],[6,2],[X],[6,/],[8,-]]
+  # score: 126
+  #
+  # []: 1フレーム
+  # n: 倒したピンの数
+  # X: ストライク
+  # /: スペア
+  # -: ガター
+  #
+  def self.score(game)
+    filtered_game = game.dup
+    filtered_game.gsub!(/-/, ':-')
+
+    flames = eval(filtered_game)
+    flames.inject(0) { |score, flame|
+      score1 = flame[0]
+      score1 = 0 if score1 == :-
+      score2 = flame[1]
+      score2 = 0 if score2 == :-
+      
+      score + score2 + score1
+    }
+  end
 end
--- spec/lib/bowling_spec.rb
+++ spec/lib/bowling_spec.rb
@@ -3,5 +3,15 @@
 require_relative "../../lib/bowling"
 
 describe Bowling do
-  
+  context "with flames that has score 2" do
+    #        1     2     3     4     5     6     7     8     9     10
+    game = "[[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1]]"
+    it { expect(Bowling.score(game)).to eq 20 }
+  end
+
+  context "with flames that has gutter" do
+    #        1     2     3     4     5     6     7     8     9     10
+    game = "[[-,-],[-,-],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1]]"
+    it { expect(Bowling.score(game)).to eq 16 }
+  end
 end
