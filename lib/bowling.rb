afa9d7456d4ee2f40f538850e01f0e1a738f9569
ice <meltedise@gmail.com>
2014-04-26 14:50:52 +0900

    rename Bowling to Bowling::Game.

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -4,46 +4,48 @@
 # frame
 # score
 
-class Bowling
-  # == Parameters:
-  #
-  # game: "[[1,1],[2,3],...]"
-  #
-  # ex.
-  # [[5,3],[7,2],[8,/],[X],[7,1],[9,-],[6,2],[X],[6,/],[8,-]]
-  # score: 126
-  #
-  # []: 1フレーム
-  # n: 倒したピンの数
-  # X: ストライク
-  # /: スペア
-  # -: ガター
-  #
-  def self.score(game)
-    filtered_game = game.dup
-    filtered_game.gsub!(%r{-}, ':-')
-    filtered_game.gsub!(%r{/}, ':/')
+module Bowling
+  class Game
+    # == Parameters:
+    #
+    # game: "[[1,1],[2,3],...]"
+    #
+    # ex.
+    # [[5,3],[7,2],[8,/],[X],[7,1],[9,-],[6,2],[X],[6,/],[8,-]]
+    # score: 126
+    #
+    # []: 1フレーム
+    # n: 倒したピンの数
+    # X: ストライク
+    # /: スペア
+    # -: ガター
+    #
+    def score(game)
+      filtered_game = game.dup
+      filtered_game.gsub!(%r{-}, ':-')
+      filtered_game.gsub!(%r{/}, ':/')
 
-    flames = eval(filtered_game)
-    score = 0
-    spare = false
-    flames.each do |flame|
-      score1 = flame[0]
-      score1 = 0 if score1 == :-
-      if spare
-        score += 10 + score1
-        spare = false
-      end
-      score2 = flame[1]
-      score2 = 0 if score2 == :-
+      flames = eval(filtered_game)
+      score = 0
+      spare = false
+      flames.each do |flame|
+        score1 = flame[0]
+        score1 = 0 if score1 == :-
+        if spare
+          score += 10 + score1
+          spare = false
+        end
+        score2 = flame[1]
+        score2 = 0 if score2 == :-
 
-      if score2 == :/
-        spare = true
-      else
-        score += score2 + score1
+        if score2 == :/
+          spare = true
+        else
+          score += score2 + score1
+        end
       end
+
+      score
     end
-
-    score
   end
 end
--- spec/lib/bowling_spec.rb
+++ spec/lib/bowling_spec.rb
@@ -2,23 +2,25 @@
 
 require_relative "../../lib/bowling"
 
-describe Bowling do
+describe Bowling::Game do
+  subject { Bowling::Game.new }
+
   context "with flames that has score 2" do
     #        1     2     3     4     5     6     7     8     9     10
     game = "[[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1]]"
-    it { expect(Bowling.score(game)).to eq 20 }
+    it { expect(subject.score(game)).to eq 20 }
   end
 
   context "with flames that has gutter" do
     #        1     2     3     4     5     6     7     8     9     10
     game = "[[-,-],[-,-],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1]]"
-    it { expect(Bowling.score(game)).to eq 16 }
+    it { expect(subject.score(game)).to eq 16 }
   end
 
   context "with flames that has spare" do
     #        1     2     3     4     5     6     7     8     9     10
     game = "[[1,1],[1,/],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1]]"
     # score   2     13    15    17    19    21    23    25    27    29
-    it { expect(Bowling.score(game)).to eq 29 }
+    it { expect(subject.score(game)).to eq 29 }
   end
 end
