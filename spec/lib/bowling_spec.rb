efd930447febb93e3d0c0def42007cb3aecc1395
ice <meltedise@gmail.com>
2014-04-26 15:21:16 +0900

    change game variable with let(:game)

---
+++ spec/lib/bowling_spec.rb
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+require_relative "../../lib/bowling"
+
+describe Bowling::Game do
+  subject { Bowling::Game.new }
+
+  context "with flames that has score 2" do
+    #        1     2     3     4     5     6     7     8     9     10
+    let(:game) { "[[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1]]" }
+    it { expect(subject.score(game)).to eq 20 }
+  end
+
+  context "with flames that has gutter" do
+    #        1     2     3     4     5     6     7     8     9     10
+    let(:game) { "[[-,-],[-,-],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1]]" }
+    it { expect(subject.score(game)).to eq 16 }
+  end
+
+  context "with flames that has spare" do
+    #        1     2     3     4     5     6     7     8     9     10
+    let(:game) { "[[1,1],[1,/],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1]]" }
+    # score   2     13    15    17    19    21    23    25    27    29
+    it { expect(subject.score(game)).to eq 29 }
+  end
+
+  context "with flames that has strike!!" do
+  end
+end
